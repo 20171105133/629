3ed4358fcc93c56e2fcc9be7c28d2a1af3b4c42c
20171105133 <164816176@qq.com>
2018-06-29 21:28:44 +0800

    读取文件信息
    创建新的文件，将需要的信息录入，读取文件信息，将文件改成csv表格形式。注意正确填写文件地址，并且注意空格和换行符的运用，以方便读取信息。

--- 629/main.cpp
+++ 629/main.cpp
@@ -7,9 +7,22 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[])
+{
+    FILE *a;
+    char s1[200];
+    char s2[200];
+    char s3[200];
+    char s4[200];
+    char s5[200];
+    char s6[200];
+    if((a=fopen("/Users/s20171105133/Desktop/6.29 next/File.csv","r"))==0)
+    {
+        printf("文件不存在\n");
+    }
+    else
+    {fscanf(a,"%s ,%s ,%s ,%s ,%s ,%s",s1,s2,s3,s4,s5,s6);
+        printf("%s\n%s\n%s\n%s\n%s\n%s\n",s1,s2,s3,s4,s5,s6);
+    }
     return 0;
 }
